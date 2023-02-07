4d9fcaae9c3a13cc71a23c02302879d07e7d6ab7
kwonso8520 <rnjstjrdnjs@dgsw.hs.kr>
2023-02-07 09:52:10 +0900

    test

--- Assets/Scirpt/Player.cs
+++ Assets/Scirpt/Player.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float Speed = 5;
-    private Rigidbody2D rigidbody;
+    Rigidbody2D rigid2D;
+    float walkForce = 10.0f;
+    float maxWalkSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hAxis = Input.GetAxisRaw("Horizontal");
-        float vAxis = Input.GetAxisRaw("Vertical");
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
 
-        Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
+     
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
-        rigidbody.velocity = inputDir * Speed;
+        
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
 
-        transform.LookAt(transform.position + inputDir);
+        
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
     }
 }
