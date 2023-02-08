e44ff345fb592010bec61b06fc8fa83b2c49eb17
Hekaline <swlee2282@gmail.com>
2023-02-08 10:44:22 +0900

    compile error fix

--- Assets/Scirpt/Button.cs
+++ Assets/Scirpt/Button.cs
@@ -1,10 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using System.ComponentModel;
-using UnityEditor.Tilemaps;
+
 
 public class Button : MonoBehaviour
 {
